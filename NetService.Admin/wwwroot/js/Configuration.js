70dad23501a314d6bc1c4355d2a7d658a147cb2c
whitejasmine1806@gmail.com <whitejasmine1806@gmail.com>
2019-03-22 20:04:21 +0700

    First Commit

---
+++ NetService.Admin/wwwroot/js/Configuration.js
@@ -0,0 +1,10 @@
+﻿var BASE_API_URL = "http://localhost:8080";
+var LOGIN_URL = "login";
+var CUSTOMER_URL = "customer";
+var ORDER_URL = "order";
+var INVENTORY_URL = "inventory";
+var STAFF_URL = "staff";
+
+var GOOGLE_API_KEY = "AIzaSyBzgvkqSdA28vGw5qvqgJdPp-3_8YEBzFo";
+var RECIPE_LEVEL_ENUM = ["", "Dễ", "Trung bình", "Khó"];
+
